28f3a208e3748a823ea108978c63cea804c4bd36
Daniyal Master <daniyalmaster693@gmail.com>
2024-08-15 19:17:09 -0400

    Declared more HTML elements in the main.js file. Also created separate modules for hiding and showing modals

--- main.js
+++ main.js
@@ -1,40 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Task Stats
+
+  const pendingNumber = document.querySelector(".pending-number");
+  const completedNumber = document.querySelector(".completed-number");
+  const dailyGoalNumber = document.querySelector(".daily-goal-number");
+
+  // Tasks Container
+
+  const tasksContainer = document.querySelector(".tasks-container");
+
+  // Creating a task fields
+
   const taskTitle = document.querySelector(".task-title-input");
   const taskDescription = document.querySelector(".task-description-input");
   const taskDueDate = document.querySelector(".task-due-date-input");
-  const addTaskButton = document.querySelector(".add-new-task");
+  const taskPriorityLevel = document.querySelector(".task-priority-level");
+  const createTask = document.querySelector(".create-task");
+
+  //  Modal
+
   const modal = document.querySelector(".modal");
   const modalOverlay = document.querySelector(".modal-overlay");
-  const createTask = document.querySelector(".create-task");
   const addTask = document.querySelector(".add-task-button");
 
-  const allTasks = [];
-  const todayTasks = [];
-  const noDateTasks = [];
+  // Modal Modules
 
-  function showModal() {
-    modal.classList.add("visible");
-    modalOverlay.classList.remove("hidden");
+  const displayModalModule = (function () {
+    function showModal() {
+      modal.classList.add("visible");
+      modalOverlay.classList.remove("hidden");
 
-    document.addEventListener("keydown", keyboardCloseModal);
-  }
-
-  function hideModal() {
-    modal.classList.remove("visible");
-    modalOverlay.classList.add("hidden");
-
-    document.removeEventListener("keydown", keyboardCloseModal);
-  }
-
-  function keyboardCloseModal() {
-    const key = event.key;
-    if (key === "Escape") {
-      hideModal();
+      document.addEventListener("keydown", hideModalModule.keyboardCloseModal);
     }
-  }
 
-  hideModal();
+    return {
+      showModal,
+    };
+  })();
 
-  addTask.addEventListener("click", showModal);
-  modalOverlay.addEventListener("click", hideModal);
+  const hideModalModule = (function () {
+    function hideModal() {
+      modal.classList.remove("visible");
+      modalOverlay.classList.add("hidden");
+
+      document.removeEventListener("keydown", keyboardCloseModal);
+    }
+
+    function keyboardCloseModal() {
+      const key = event.key;
+      if (key === "Escape") {
+        hideModal();
+      }
+    }
+
+    hideModal();
+
+    return {
+      hideModal,
+      keyboardCloseModal,
+    };
+  })();
+
+  addTask.addEventListener("click", displayModalModule.showModal);
+  modalOverlay.addEventListener("click", hideModalModule.hideModal);
 });
